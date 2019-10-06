9dc8268e07dc84bbf21a0cb4636326e1ae9ef32c
xkhottabx <xkhottabx@gmail.com>
2019-10-05 21:31:18 -0400

    backend

---
+++ src/test/java/com/verisign/utilities/APIUtils.java
@@ -0,0 +1,31 @@
+package com.verisign.utilities;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+public class APIUtils {
+
+    private static Response response;
+
+    public static void setResponse(Response res) {
+        response = res;
+    }
+
+    public static Response getResponse() {
+        return response;
+    }
+
+    public static String generateToken() {
+        Response response = given().queryParams("email", ConfigurationReader.getProperty("email"),
+                "password",ConfigurationReader.getProperty("password") )
+                .when().get(ConfigurationReader.getProperty("baseuri")+"/sign");
+        // String token = response.body().jsonPath().getString("accessToken");
+        JsonPath jsonPath = response.body().jsonPath();
+        String token = jsonPath.getString("accessToken");
+        return token;
+
+    }
+
+
+}
---
+++ src/test/java/com/verisign/utilities/DBType.java
@@ -0,0 +1,5 @@
+package com.verisign.utilities;
+
+public enum DBType {
+    ORACLE, MYSQL
+}
